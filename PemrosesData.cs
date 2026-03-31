74025de4415b0e97ae436a56497d3ed9d58fa7ca
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-03-31 08:02:05 +0700

    fix

---
+++ PemrosesData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul5_103022400129
+{
+    public class PemrosesData
+    {
+        public T DapatkanNilaiTerbesar<T>(T x, T y, T z)
+        {
+            dynamic a = x;
+            dynamic b = y;
+            dynamic c = z;
+            dynamic temp = a;
+
+            if (temp < b)
+            {
+                temp = b;
+            }
+            if (temp < c)
+            {
+                temp = c;
+            }
+
+
+            return temp;
+        }
+    }
+}
